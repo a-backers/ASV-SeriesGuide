884778570b32c972c5b7219724cb1a33fe716e20
a-backers <107916060+a-backers@users.noreply.github.com>
2022-07-08 11:06:26 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,10 +96,10 @@ nexusPublishing {
     }
 }
 
-tasks.register("clean", Delete::class) {
-    group = "build"
-    delete(rootProject.buildDir)
-}
+//tasks.register("clean", Delete::class) {
+//    group = "build"
+//    delete(rootProject.buildDir)
+//}
 
 tasks.wrapper {
     //noinspection UnnecessaryQualifiedReference
